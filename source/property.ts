2f908c68514b6f41aecfefdf233835d69f243016
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2024-01-11 10:54:29 +0100

    Fix shorthand styles not returning properties

--- source/property.ts
+++ source/property.ts
@@ -12,6 +12,10 @@ export class StyleProperty {
 	 * You may return an altered version of this property depending on the use
 	 */
 	use(selector: string): StyleProperty | StyleProperty[] | undefined | null {
+		if (this.children) {
+			return this.children;
+		}
+
 		return this;
 	}
 
