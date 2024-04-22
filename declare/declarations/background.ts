3661dfaf86e68c64afc0712cfc2ffbe6e2255380
younesxd <eltaief.younes@hotmail.com>
2024-04-22 15:57:27 +0200

    Add background position property

--- declare/declarations/background.ts
+++ declare/declarations/background.ts
@@ -4,6 +4,7 @@ import { ShorthandDeclaration } from "../builders/shorthand";
 import { colorValue } from "./color";
 import { imageSource, length, percentage } from "./primitives";
 import { gradient } from "./gradient";
+import { MethodDeclaration } from "../builders/method";
 
 export const backgroundImageSource = new TypeDeclaration(imageSource, gradient);
 
@@ -32,11 +33,28 @@ export const backgroundRepeat = new PropertyTypeDeclaration({
 	type: backgroundRepeatType.spread()
 }, "${this.type.join(' ')}");
 
-export const backgroundPositionType = new TypeDeclaration('top', 'left', 'right', 'bottom', 'center');
+export const backgroundPositionDirectionalOffsetLength = new TypeDeclaration(length, percentage);
 
-export const backgroundPosition = new PropertyTypeDeclaration({
-	types: backgroundPositionType.spreadArray()
-}, "${this.types.map(type => type.join(' ')).join(',')}");
+const offsetSide = (side: string) => module.exports[`${side}Offset`] = new MethodDeclaration({
+	offset: backgroundPositionDirectionalOffsetLength.optional()
+}, 'this.offset = offset', side + '${this.offset ? ` ${this.offset}` : \'\'}');
+
+export const backgroundPositionXType = new TypeDeclaration('left', 'right', 'center', percentage, length, offsetSide('left'), offsetSide('right'));
+
+export const backgroundPositionX = new PropertyTypeDeclaration({
+	type: backgroundPositionXType.spread()
+}, "${this.type.join(' ')}");
+
+export const backgroundPositionYType = new TypeDeclaration('top', 'bottom', 'center', percentage, length, offsetSide('top'), offsetSide('bottom'));
+
+export const backgroundPositionY = new PropertyTypeDeclaration({
+	type: backgroundPositionYType.spread()
+}, "${this.type.join(' ')}");
+
+export const backgroundPosition = new ShorthandDeclaration([
+	backgroundPositionX,
+	backgroundPositionY
+]);
 
 export const backgroundAttachmentType = new TypeDeclaration('scroll', 'fixed', 'local');
 
