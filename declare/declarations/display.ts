71d692ae1d2992c2e48e58048563d8b65b91a33a
younesxd <eltaief.younes@hotmail.com>
2024-04-23 10:59:38 +0200

    Add all possible display modes

--- declare/declarations/display.ts
+++ declare/declarations/display.ts
@@ -1,11 +1,22 @@
 import { TypeDeclaration } from "../builders/type";
 import { PropertyTypeDeclaration } from "../builders/property";
 
-export const displayMode = new TypeDeclaration('block', 'inline', 'flex', 'grid', 'flow-root', 'none', 'contents');
+// All possible combinations of the display property
+export const displayOutsideModes = new TypeDeclaration('block', 'inline');
+export const displayInsideModes = new TypeDeclaration('flow', 'flow-root', 'table', 'flex', 'grid', 'ruby', 'list-item');
+
+export const displayCombinationModes = new TypeDeclaration('block flow', 'inline flow', 'block flow-root', 'inline flow-root', 'block table', 'inline table', 'block flex', 'inline flex', 'block grid', 'inline grid', 'block ruby', 'inline ruby', 'block list-item', 'inline list-item');
+
+export const displayInternalModes = new TypeDeclaration('table-row-group', 'table-header-group', 'table-footer-group', 'table-row', 'table-cell', 'table-column-group', 'table-column', 'table-caption', 'ruby-base', 'ruby-text', 'ruby-base-container', 'ruby-text-container');
+export const displayBoxModes = new TypeDeclaration('none', 'contents');
+export const displayPrecomposedModes = new TypeDeclaration('inline-block', 'inline-table', 'inline-flex', 'inline-grid');
+
+export const displayModes = new TypeDeclaration(displayOutsideModes, displayInsideModes, displayCombinationModes, displayInternalModes, displayBoxModes, displayPrecomposedModes);
 
 export const display = new PropertyTypeDeclaration(
 	{
-		mode: displayMode.spread()
+		mode: displayModes.single()
+
 	},
-	"${this.mode.join('-')}"
+	"${this.mode}"
 );
