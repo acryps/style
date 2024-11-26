d92cf185372f07e0b6c1bfaed82ea612ce0882e8
Levi Hechenberger Di Valentino <levi.hechenberger@acryps.com>
2024-11-26 16:54:45 +0100

    Fix scrollbar-width name, add peer properties

--- declare/declarations/scroll.ts
+++ declare/declarations/scroll.ts
@@ -1,5 +1,6 @@
 import { PropertyTypeDeclaration } from "../builders/property";
 import { TypeDeclaration } from "../builders/type";
+import { colorValue } from "./color";
 
 export const scrollSnapAxis = new TypeDeclaration('none', 'x', 'y', 'block', 'inline', 'both');
 export const scrollSnapForceMode = new TypeDeclaration('mandatory', 'proximity');
@@ -27,8 +28,19 @@ export const overscrollBehavior = new PropertyTypeDeclaration({
 	mode: overscrollBehaviorMode.single()
 }, "${this.mode}");
 
-export const scrollBarWidthMode = new TypeDeclaration('auto', 'thin', 'none');
+export const scrollbarWidthMode = new TypeDeclaration('auto', 'thin', 'none');
 
-export const scrollBarWidth = new PropertyTypeDeclaration({
-	mode: scrollBarWidthMode.single()
+export const scrollbarWidth = new PropertyTypeDeclaration({
+	mode: scrollbarWidthMode.single()
 }, "${this.mode}");
+
+export const scrollbarGutterMode = new TypeDeclaration('auto', 'stable', 'both-edges');
+
+export const scrollbarGutter = new PropertyTypeDeclaration({
+	modes: scrollbarGutterMode.spread()
+}, "${this.modes.join(' ')}");
+
+export const scrollbarColor = new PropertyTypeDeclaration({
+	thumb: colorValue.single(),
+	track: colorValue.optional()
+}, "${this.thumb}${this.track ? ` ${this.track}` : ''}");
