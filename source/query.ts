b2ced86bfdd851b2ed1ec06133a513bc83724b93
Eggsecuter <adrian.habeler@gmail.com>
2025-07-01 09:03:03 +0200

    Add pseudo elements, Fix selectors

--- source/query.ts
+++ source/query.ts
@@ -6,14 +6,18 @@ export type StyleInsert = { toStyleGroup(): StyleGroup } | { toStyleProperty(): 
 export type StyleSelectorBody = StyleProperty | StyleGroup | StyleInsert | AtRule | StyleSelectorBody[];
 
 type Selector = string | (() => StyleGroup);
+type ViewTransition = '*' | 'root' | (() => StyleGroup);
 type NTHPattern = 'even' | 'odd' | number | {
 	step?: number;
 	offset?: number;
 }
 
-const stringifySelector = (selector: Selector) => typeof selector == 'string' ? selector : selector().selector;
 
-const stringifyNTHPattern = (pattern: NTHPattern, filter: Selector) => {
+const stringifySelector = (selector: Selector) => typeof selector == 'string' ? selector : selector().selector;
+const stringifySelectors = (selectors: Selector[]) => selectors.map(selector => stringifySelector(selector)).join('');
+const stringifyViewTransition = (viewTransition: ViewTransition) => typeof viewTransition == 'string' ? viewTransition : viewTransition().selector;
+
+const stringifyNTHPattern = (pattern: NTHPattern, ...selectors: Selector[]) => {
 	let stringified: string;
 
 	if (typeof pattern == 'string') {
@@ -38,8 +42,8 @@ const stringifyNTHPattern = (pattern: NTHPattern, filter: Selector) => {
 		stringified = combination;
 	}
 
-	if (filter) {
-		stringified += ` of ${stringifySelector(filter)}`;
+	if (selectors.length) {
+		stringified += ` of ${stringifySelectors(selectors)}`;
 	}
 
 	return stringified;
@@ -54,20 +58,20 @@ export const universal = () => {
 	return style('*');
 }
 
-export const descendant = (selector: Selector) => {
-	return style(` ${stringifySelector(selector)}`);
+export const descendant = (selector: string) => {
+	return style(` ${selector}`);
 }
 
-export const child = (selector: Selector) => {
-	return style(`>${stringifySelector(selector)}`);
+export const child = (selector: string) => {
+	return style(`>${selector}`);
 }
 
-export const adjacentSibling = (selector: Selector) => {
-	return style(`+${stringifySelector(selector)}`);
+export const adjacentSibling = (selector: string) => {
+	return style(`+${selector}`);
 }
 
-export const sibling = (selector: Selector) => {
-	return style(`~${stringifySelector(selector)}`);
+export const sibling = (selector: string) => {
+	return style(`~${selector}`);
 }
 
 export const id = (selector: string) => {
@@ -128,12 +132,12 @@ export const checked = () => {
 	return style(':checked');
 }
 
-export const current = (...selectors: Selector[]) => {
+export const current = (...selectors: string[]) => {
 	if (!selectors.length) {
 		return style(':current');
 	}
 
-	return style(`:current(${selectors.map(selector => stringifySelector(selector)).join(', ')})`);
+	return style(`:current(${selectors.join(', ')})`);
 }
 
 export const defaultSelector = () => {
@@ -184,32 +188,32 @@ export const fullscreen = () => {
 	return style(':fullscreen');
 }
 
-export const future = (...selectors: Selector[]) => {
+export const future = (...selectors: string[]) => {
 	if (!selectors.length) {
 		return style(':future');
 	}
 
-	return style(`:future(${selectors.map(selector => stringifySelector(selector)).join(', ')})`);
+	return style(`:future(${selectors.join(', ')})`);
 }
 
 export const hasSlotted = () => {
 	return style(':hasSlotted');
 }
 
-export const has = (selector: Selector) => {
-	return style(`:has(${stringifySelector(selector)})`);
+export const has = (...selectors: Selector[]) => {
+	return style(`:has(${stringifySelectors(selectors)})`);
 }
 
-export const host = (selector?: Selector) => {
-	if (selector) {
-		return style(`:host(${stringifySelector(selector)})`);
+export const host = (...selectors: Selector[]) => {
+	if (stringifySelectors.length) {
+		return style(`:host(${stringifySelectors(selectors)})`);
 	} else {
 		return style(':host');
 	}
 }
 
-export const hostContext = (selector: Selector) => {
-	return style(`:host-context(${stringifySelector(selector)})`);
+export const hostContext = (...selectors: Selector[]) => {
+	return style(`:host-context(${stringifySelectors(selectors)})`);
 }
 
 export const hover = () => {
@@ -228,8 +232,8 @@ export const invalid = () => {
 	return style(':invalid');
 }
 
-export const is = (...selectors: Selector[]) => {
-	return style(`:is(${selectors.map(selector => stringifySelector(selector)).join(', ')})`);
+export const is = (...selectorsList: Selector[][]) => {
+	return style(`:is(${selectorsList.map(selectors => stringifySelectors(selectors)).join(', ')})`);
 }
 
 export const lang = (...languages: string[]) => {
@@ -260,16 +264,16 @@ export const muted = () => {
 	return style(':muted');
 }
 
-export const not = (...selectors: Selector[]) => {
-	return style(`:not(${selectors.map(selector => stringifySelector(selector)).join(', ')})`);
+export const not = (...selectorsList: Selector[][]) => {
+	return style(`:not(${selectorsList.map(selectors => stringifySelectors(selectors)).join(', ')})`);
 }
 
-export const nthChild = (pattern: NTHPattern, filter?: Selector) => {
-	return style(`:nth-child(${stringifyNTHPattern(pattern, filter)})`);
+export const nthChild = (pattern: NTHPattern, ...selectors: Selector[]) => {
+	return style(`:nth-child(${stringifyNTHPattern(pattern, ...selectors)})`);
 }
 
-export const nthLastChild = (pattern: NTHPattern, filter?: Selector) => {
-	return style(`:nth-last-child(${stringifyNTHPattern(pattern, filter)})`);
+export const nthLastChild = (pattern: NTHPattern, ...selectors: Selector[]) => {
+	return style(`:nth-last-child(${stringifyNTHPattern(pattern, ...selectors)})`);
 }
 
 export const nthLastOfType = (pattern: NTHPattern) => {
@@ -300,12 +304,12 @@ export const outOfRange = () => {
 	return style(':out-of-range');
 }
 
-export const past = (...selectors: Selector[]) => {
+export const past = (...selectors: string[]) => {
 	if (!selectors.length) {
 		return style(':past');
 	}
 
-	return style(`:past(${selectors.map(selector => stringifySelector(selector)).join(', ')})`);
+	return style(`:past(${selectors.join(', ')})`);
 }
 
 export const paused = () => {
@@ -392,15 +396,123 @@ export const volumeLocked = () => {
 	return style(':volume-locked');
 }
 
-export const where = (...selectors: Selector[]) => {
-	return style(`:where(${selectors.map(selector => stringifySelector(selector)).join(', ')})`);
+export const where = (...selectorsList: Selector[][]) => {
+	return style(`:where(${selectorsList.map(selectors => stringifySelectors(selectors)).join(', ')})`);
 }
 
 // pseudo elements
+export const after = () => {
+	return style('::after');
+}
+
+export const backdrop = () => {
+	return style('::backdrop');
+}
+
 export const before = () => {
 	return style('::before');
 }
 
-export const after = () => {
-	return style('::after');
+export const checkmark = () => {
+	return style('::checkmark');
+}
+
+export const column = () => {
+	return style('::column');
+}
+
+export const cue = () => {
+	return style('::cue');
+}
+
+export const detailsContent = () => {
+	return style('::details-content');
+}
+
+export const fileSelectorButton = () => {
+	return style('::file-selector-button');
+}
+
+export const firstLetter = () => {
+	return style('::first-letter');
+}
+
+export const firstLine = () => {
+	return style('::first-line');
+}
+
+export const grammarError = () => {
+	return style('::grammar-error');
+}
+
+export const highlight = (name: string) => {
+	return style(`::highlight(${name})`);
+}
+
+export const marker = () => {
+	return style('::marker');
+}
+
+export const part = (...indents: string[]) => {
+	return style(`::part(${indents.join(' ')})`);
+}
+
+export const pickerIcon = () => {
+	return style('::picker-icon');
+}
+
+export const picker = (indent: string) => {
+	return style(`::picker(${indent})`);
+}
+
+export const placeholder = () => {
+	return style('::placeholder');
+}
+
+export const scrollButton = (direction: '*' | 'down' | 'left' | 'right' | 'up' | 'block-end' | 'block-start' | 'inline-end' | 'inline-start') => {
+	return style(`::scroll-button(${direction})`);
+}
+
+export const scrollMarker = () => {
+	return style('::scroll-marker');
+}
+
+export const scrollMarkerGroup = () => {
+	return style('::scroll-marker-group');
+}
+
+export const selection = () => {
+	return style('::selection');
+}
+
+export const slotted = (...selectors: Selector[]) => {
+	return style(`::slotted(${stringifySelectors(selectors)})`);
+}
+
+export const spellingError = () => {
+	return style('::spelling-error');
+}
+
+export const targetText = () => {
+	return style('::target-text');
+}
+
+export const viewTransition = () => {
+	return style('::view-transition');
+}
+
+export const viewTransitionGroup = (selector: ViewTransition) => {
+	return style(`::view-transition-group(${stringifyViewTransition(selector)})`);
+}
+
+export const viewTransitionImagePair = (selector: ViewTransition) => {
+	return style(`::view-transition-image-pair(${stringifyViewTransition(selector)})`);
+}
+
+export const viewTransitionNew = (selector: ViewTransition) => {
+	return style(`::view-transition-new(${stringifyViewTransition(selector)})`);
+}
+
+export const viewTransitionOld = (selector: ViewTransition) => {
+	return style(`::view-transition-old(${stringifyViewTransition(selector)})`);
 }
